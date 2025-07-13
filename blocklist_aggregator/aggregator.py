9e4248982d738fb6a32bcb542325cafbf815d513
dmachard <5562930+dmachard@users.noreply.github.com>
2025-07-13 16:21:44 +0200

    fix: no more return empty list on fetch

--- blocklist_aggregator/aggregator.py
+++ blocklist_aggregator/aggregator.py
@@ -77,12 +77,12 @@ def fetch(cfg_update=None, cfg_filename=None):
             else:
                 if r.status_code != 200:
                     logging.error("http error: %s" % r.status_code)
-                    return []
+                    #return []
                 else:
                     domains = inspect_source(s["pattern"], r.text)
                     if len(domains) == 0:
                         logging.error("no domains extracted for: %s" % u)
-                        return []
+                    #    return []
                     domains_bl.extend(domains)  
             
     # add more domains to the blocklist ?
